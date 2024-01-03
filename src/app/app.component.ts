043d2185597921417b48a87f79689ff133596eeb
Mathis Gasparotto <mgasparotto@getinov.com>
2024-01-03 16:34:54 +0100

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,7 @@ import { ENTITIES } from './mock-entity-list'
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>Welcome to {{entitySelected.name}}!</h1>
-  <h2>Liste des entités</h2>
-  <ul>
-    <li *ngFor="let entity of entities">{{entity.name}}</li>
-  </ul>
-  `,
+  templateUrl: 'app.component.html',
   styles: []
 })
 
@@ -20,15 +14,20 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     console.table(this.entities)
-    this.entitySelected = this.getRandomEntity(this.entities)
-    this.selectEntity(this.entitySelected)
+    const entity = this.getRandomEntity(this.entities)
+    this.selectEntity(entity)
   }
 
   getRandomEntity(entityList: Entity[]): Entity {
     return entityList[Math.floor(Math.random() * entityList.length)]
   }
 
-  selectEntity(entity: Entity) {
-    console.log(`Vous avez cliqué sur l'entité ${entity.name}`)
+  selectEntity(entity: Entity|MouseEvent) {
+    if (entity instanceof MouseEvent) {
+      this.entitySelected = this.entities[Number((entity.target as HTMLInputElement).value)]
+    } else {
+      this.entitySelected = entity
+    }
+    console.log(`Vous avez cliqué sur l'entité ${this.entitySelected.name}`)
   }
 }
